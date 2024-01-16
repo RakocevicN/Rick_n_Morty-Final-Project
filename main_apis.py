5f97e4199a7b5590a5f420f6d9e1e0c97e5a1a2c
RakocevicN <nikolarakocevic52@gmail.com>
2024-01-16 03:33:04 +0100

    napokon adas

--- main_apis.py
+++ main_apis.py
@@ -4,28 +4,28 @@ Docu to bi improved
 import requests
 
 # Documentation can be found here: https://rickandmortyapi.com/documentation/#introduction
-main_api = "https://rickandmortyapi.com/api"
+glavni = "https://rickandmortyapi.com/api"
 
 
 def get_episode(episode_id):
-    episode_url = f"{main_api}/episode/{episode_id}"
+    episode_url = f"{glavni}/episode/{episode_id}"
     response = requests.get(episode_url)
     return response.json()
 
 
 def get_character_by_id(character_id):
-    id_url = f"{main_api}/character/{character_id}"
+    id_url = f"{glavni}/character/{character_id}"
     response = requests.get(id_url)
     return response.json()
 
 
 def get_all_locations():
-    locations_url = f"{main_api}/location"
+    locations_url = f"{glavni}/location"
     response = requests.get(locations_url)
     return response.json()
 
 
 def get_location_info(location_id):
-    location_url = f"{main_api}/location/{location_id}"
+    location_url = f"{glavni}/location/{location_id}"
     response = requests.get(location_url)
     return response.json()
