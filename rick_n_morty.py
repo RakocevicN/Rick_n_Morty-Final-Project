3d729b78a50a57799ab29bbcdc51e319a47aac92
RakocevicN <nikolarakocevic52@gmail.com>
2024-01-16 02:27:46 +0100

    commit message

--- rick_n_morty.py
+++ rick_n_morty.py
@@ -1,8 +1,10 @@
+""""
+Docu to bi improved
+"""
 import requests
 from config import main_api
 
 
-# To get single character by adding the ID as a parameter, e.g./character/2
 def get_character_by_id(character_id):
     id_url = f"{main_api}/character/{character_id}"
     response = requests.get(id_url)
