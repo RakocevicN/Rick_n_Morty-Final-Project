5f3e5914cc516e7bb38ba21d47bff577a6a1b96e
RakocevicN <nikolarakocevic52@gmail.com>
2024-01-19 03:07:43 +0100

    improved test_execution & readme for tests

--- test_execution.py
+++ test_execution.py
@@ -1,46 +1,48 @@
 import click
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
 """
 Run different sets of tests:
-    --test_episode/--test_id/--test_location/--all_tests (bool):
-    Set these flags to True to execute the corresponding tests, or False to skip them.
-@click.command() decorator is used to define CLI commands.
-@click.option() decorators are used to define command-line options.  
+    --test_episode/--test_id/--test_location/--all_tests
+    Set these flags to True to execute the corresponding tests
 For more about click, please visit: https://click.palletsprojects.com/en/8.1.x/
 """
 
 
 @click.command()
-@click.option('--test_episode', is_flag=True, help='Run episode tests. Use --test_episode')
-@click.option('--test_id', is_flag=True, help='Run ID tests. Use --test_id')
-@click.option('--test_location', is_flag=True, help='Run location tests. Use --test_location')
+@click.option('--test_episode', is_flag=True, help='To run episode tests. Use --test_episode')
+@click.option('--test_id', is_flag=True, help='To run ID tests. Use --test_id')
+@click.option('--test_location', is_flag=True, help='To run location tests. Use --test_location')
 @click.option('--all_tests', is_flag=True, help='To run all tests. Use --all_tests')
 def run_tests(test_episode, test_id, test_location, all_tests):
+    test_types = []
+
     if all_tests:
-        run_all_tests()
-    elif test_episode:
-        run_specific_tests('episode')
-    elif test_id:
-        run_specific_tests('id')
-    elif test_location:
-        run_specific_tests('location')
+        test_types = ['episode', 'id', 'location']
+        max_workers = 9
     else:
-        click.echo("No test option specified. Please see --test_episode, --test_id, --test_location, or --all.")
+        max_workers = 5
+        if test_episode:
+            test_types.append('episode')
+        if test_id:
+            test_types.append('id')
+        if test_location:
+            test_types.append('location')
+
+    if not test_types:
+        click.echo("No test option specified. Please see --test_episode, --test_id, --test_location, or --all_tests")
+    else:
+        execute_tests_concurrently(test_types, max_workers)
 
 
-def run_all_tests():
-    try:
-        subprocess.run(["pytest", "tests"])
-    except Exception as problem:
-        print(f"Problem while running all tests: {problem}")
+def execute_tests(test_type=None):
+    subprocess.run(["pytest", "tests" if test_type is None else f"tests/test_{test_type}.py"])
 
 
-def run_specific_tests(test_type):
-    try:
-        subprocess.run(["pytest", f"tests/test_{test_type}.py"])
-    except Exception as problem:
-        print(f"Problem while running {test_type} test: {problem}")
+def execute_tests_concurrently(test_types, max_workers):
+    with ThreadPoolExecutor(max_workers=max_workers) as ex:
+        ex.map(execute_tests, test_types)
 
 
 if __name__ == '__main__':
